f3beadf66edc9160d9ab1aa2d2218bf68a714886
mjfdhh <142124909+mjfdhh@users.noreply.github.com>
2024-08-21 19:05:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ global.media = {
  profil: 'https://i.ibb.co/3Fh9V6p/avatar-contact.png',
  did: 'https://telegra.ph/file/fdc1a8b08fe63520f4339.jpg',
  rules: 'https://telegra.ph/file/afcfa712bd09f4fcf027a.jpg',
- thumbnail: 'https://telegra.ph/file/161c0a22c03f7859c7599.jpg',
- thumb: 'https://telegra.ph/file/161c0a22c03f7859c7599.jpg',
+ thumbnail: 'https://telegra.ph/file/3ca4c1656c27a98157090.jpg',
+ thumb: 'https://telegra.ph/file/3ca4c1656c27a98157090.jpg',
  logo: 'https://telegra.ph/file/fdc1a8b08fe63520f4339.jpg',
  unReg: 'https://telegra.ph/file/ef02d1fdd59082d05f08d.jpg',
  registrasi: 'https://telegra.ph/file/0169f000c9ddc7c3315ff.jpg',
@@ -46,7 +46,7 @@ global.media = {
 
 // Sosmed
 global.url = {
- sig: 'https://instagram.com/noureddine_ouafy',
+ sig: 'https://www.instagram.com/benaddi_404?igsh=YzljYTk1ODg3Zg==',
  sgh:  'https://github.com/noureddineouafy',
  sgc: 'https://chat.whatsapp.com/DABxracGsi5CQMd5d1N1j7'
 }
